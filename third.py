85f1a2643bd531c9c6139a4f81550b10c4c43e2e
hwax0r <52138272+hwax0r@users.noreply.github.com>
2019-09-25 19:07:21 +0300

    Update third.py
     В строке, состоящей из слов, разделенных пробелом, найти самое длинное слово.

--- third.py
+++ third.py
@@ -1,3 +1,4 @@
+# В строке, состоящей из слов, разделенных пробелом, найти самое длинное слово.
 inp_str = "The father of a British toddler who went missing thirty eight years ago in Germany has said he never gave up hope that she might still be alive as police arrested a man over her disappearance Military police announced yesterday that they were questioning a man in connection with the case of Katrice Lee, who vanished from a supermarket on her second birthday near a British military base in Germany in November one thousand eight hundred and one Officers reportedly began searching a terraced house in the Moredon area of Swindon in Wiltshire on Monday and made an arrest at the property the next day"
 inp_str = inp_str.split()
 print(f'input: {inp_str}')
